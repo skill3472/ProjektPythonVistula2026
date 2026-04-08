12c11dde37bd95ad8365f7aa272c9a50ddb06e58
Maksymilian Tym <tymszczur@gmail.com>
2026-04-08 20:48:46 +0200

    chore: Ruff formatting

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import kagglehub
 import pandas as pd
 from pathlib import Path
 
-path: Path = Path(kagglehub.dataset_download("samlearner/letterboxd-movie-ratings-data"))
+path: Path = Path(
+    kagglehub.dataset_download("samlearner/letterboxd-movie-ratings-data")
+)
 
-movie_data = pd.read_csv(path / "movie_data.csv", on_bad_lines='skip', engine='python')
-ratings_data = pd.read_csv(path / "ratings_export.csv", on_bad_lines='skip', engine='python')
-users_data = pd.read_csv(path / "users_export.csv", on_bad_lines='skip', engine='python')
+movie_data = pd.read_csv(path / "movie_data.csv", on_bad_lines="skip", engine="python")
+ratings_data = pd.read_csv(
+    path / "ratings_export.csv", on_bad_lines="skip", engine="python"
+)
+users_data = pd.read_csv(
+    path / "users_export.csv", on_bad_lines="skip", engine="python"
+)
 
 print("Dataframes loaded!")
 print(f"movie_data: {movie_data.shape}")
