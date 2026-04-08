a0b4ebb796f0af74d1efcd79493e88ee3fcf1f56
Maksymilian Tym <tymszczur@gmail.com>
2026-04-08 20:47:18 +0200

    feat: Added dataset downloading and loading it to dataframes

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from projektpythonvistula2026!")
+import kagglehub
+import pandas as pd
+from pathlib import Path
 
+path: Path = Path(kagglehub.dataset_download("samlearner/letterboxd-movie-ratings-data"))
 
-if __name__ == "__main__":
-    main()
+movie_data = pd.read_csv(path / "movie_data.csv", on_bad_lines='skip', engine='python')
+ratings_data = pd.read_csv(path / "ratings_export.csv", on_bad_lines='skip', engine='python')
+users_data = pd.read_csv(path / "users_export.csv", on_bad_lines='skip', engine='python')
+
+print("Dataframes loaded!")
+print(f"movie_data: {movie_data.shape}")
+print(f"ratings_data: {ratings_data.shape}")
+print(f"users_data: {users_data.shape}")
